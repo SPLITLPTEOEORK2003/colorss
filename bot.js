2712b27b3a63cd4cb75040dc6e0d80b9386497fd
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-12-14 16:07:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready',  () => {
 var prefix = '.';
 
 client.on('message', message => {
-    if(message.content === prefix + 'cr') {
+    if(message.content === prefix + 'ca') {
                          if(!message.channel.guild) return message.channel.send('**This Commnad only For Servers !**'); 
          if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('**You Dont Have** `ADMINISTRATOR` **premission**').then(msg => msg.delete(6000))
       message.guild.createRole({
